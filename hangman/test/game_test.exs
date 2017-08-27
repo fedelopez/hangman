eb23fed79fe4a95083842ddcf72fa430a7518973
Fede Lopez <fede.lopez@gmail.com>
2017-08-27 21:31:47 +1000

    Add dictionary and hangman projects

---
+++ hangman/test/game_test.exs
@@ -0,0 +1,98 @@
+defmodule HangmanGameTest do
+  use ExUnit.Case
+  alias Hangman.Game
+
+  test "new_game returns structure" do
+    game = Game.new_game()
+    assert game.turns_left == 7
+    assert game.game_state == :initializing
+    assert length(game.letters) > 0
+  end
+
+  test "letters are all lowercase" do
+    game = Game.new_game()
+    chars = Enum.map(game.letters, &(String.to_charlist(&1)))
+
+    Enum.each(chars, fn(x) -> assert ?x > 96 end)
+    Enum.each(chars, fn(x) -> assert ?x < 123 end)
+  end
+
+  test "state isn't changed for won or lost game" do
+    for state <- [:won, :lost] do
+      game = Game.new_game() |> Map.put(:game_state, state)
+      assert { ^game, _ } = Game.make_move(game, "x")
+    end
+  end
+
+  test "first occurrence of letter is not already used" do
+    game = Game.new_game()
+    {game, _tally} = Game.make_move(game, "x")
+    assert game.game_state != :already_used
+  end
+
+  test "second occurrence of letter is not already used" do
+    game = Game.new_game()
+    {game, _tally} = Game.make_move(game, "x")
+    {game, _tally} = Game.make_move(game, "x")
+    assert game.game_state == :already_used
+  end
+
+  test "a good guess is recognized" do
+    game = Game.new_game("wibble")
+    {game, tally} = Game.make_move(game, "w")
+    assert game.game_state == :good_guess
+    assert game.turns_left == 7
+  end
+
+  test "a guessed word is a won game" do
+    game = Game.new_game("house")
+    {game, tally} = Game.make_move(game, "e")
+    assert game.game_state == :good_guess
+    assert game.turns_left == 7
+    {game, tally} = Game.make_move(game, "s")
+    assert game.game_state == :good_guess
+    assert game.turns_left == 7
+    {game, tally} = Game.make_move(game, "h")
+    assert game.game_state == :good_guess
+    assert game.turns_left == 7
+    {game, tally} = Game.make_move(game, "o")
+    assert game.game_state == :good_guess
+    assert game.turns_left == 7
+    {game, tally} = Game.make_move(game, "u")
+    assert game.game_state == :won
+    assert game.turns_left == 7
+  end
+
+  test "bad guess is recognized" do
+    game = Game.new_game("wibble")
+    {game, tally} = Game.make_move(game, "z")
+    assert game.game_state == :bad_guess
+    assert game.turns_left == 6
+  end
+
+  test "lost game is recognized" do
+    game = Game.new_game("w")
+    {game, tally} = Game.make_move(game, "a")
+    {game, tally} = Game.make_move(game, "b")
+    {game, tally} = Game.make_move(game, "c")
+    {game, tally} = Game.make_move(game, "d")
+    {game, tally} = Game.make_move(game, "e")
+    {game, tally} = Game.make_move(game, "f")
+    {game, tally} = Game.make_move(game, "g")
+    assert game.game_state == :lost
+    assert game.turns_left == 0
+  end
+
+  test "reveal guessed underscores not guessed letters" do
+    game = Game.new_game("house")
+    {game, tally} = Game.make_move(game, "e")
+    {game, tally} = Game.make_move(game, "h")
+    {game, tally} = Game.make_move(game, "a")
+
+    tally = Game.tally(game)
+    assert Map.fetch!(tally, :game_state) == :bad_guess
+    assert Map.fetch!(tally, :turns_left) == 6
+    assert Map.fetch!(tally, :letters) == ["h", "_", "_", "_", "e"]
+  end
+
+end
